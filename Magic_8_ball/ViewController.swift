bd1d8ef9c7da9570834479324fd838f6939beced
Eddy Caldas <eddycaldas@live.com>
2017-11-18 22:44:28 -0700

    change color

---
+++ Magic_8_ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic_8_ball
+//
+//  Created by Eddy Caldas on 11/18/17.
+//  Copyright © 2017 Eddy Caldas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
